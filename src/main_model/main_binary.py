efbcd54a584e3db4a8026f6fd361515616b4e928
Delta0019 <125425538+Delta0019@users.noreply.github.com>
2024-07-16 16:20:05 +0800

    判断是否进决赛

---
+++ src/main_model/main_binary.py
@@ -0,0 +1,43 @@
+from data_processing import (load_data, prepare_data, oversample_data,
+                             split_data, expand_data, transform_to_binary)
+from model_training import train_mlp_classifier, train_voting_classifier
+from evaluation_binary import evaluate_model_binary, cross_validate_model_binary
+from prediction import predict_new_data
+
+
+def main():
+    # Load and prepare data
+    data = load_data('cleaned_all_data_processed.csv')
+    X, y = prepare_data(data)
+
+    # Converted into a binary classification problem if we need
+    y = transform_to_binary(y)
+
+    X_train, X_test, y_train, y_test = split_data(X, y)
+    X_train, y_train = oversample_data(X_train, y_train)
+    X_test = expand_data(X_test, 10)
+    y_test = expand_data(y_test, 10)
+
+    # data_argument
+    print("Load and prepare data completed")
+
+    # Train classifiers
+    print("Start training...")
+    mlp_classifier = train_mlp_classifier(X_train, y_train)
+    voting_classifier = train_voting_classifier(X_train, y_train, mlp_classifier)
+
+    # Evaluate model
+    print("Start evaluating...")
+    evaluate_model_binary(voting_classifier, X_test, y_test)
+
+    # Predict on new data
+    print("Predict on new data...")
+    predict_new_data(voting_classifier, 'candidate_processed4.csv')
+
+    # Cross-validate model
+    print("Cross-validate model..")
+    cross_validate_model_binary(voting_classifier, X, y)
+
+
+if __name__ == "__main__":
+    main()
